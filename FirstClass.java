ccf9aa5f556aed42218be513c5edec9a46aa4353
NadishUmair <nadishumair403@gmail.com>
2024-10-25 17:36:10 +0500

    loops

--- FirstClass.java
+++ FirstClass.java
@@ -108,21 +108,146 @@
 // }
 
 
-// ################# table Of  Number ####################
+// ################# Table Of  Number ####################
+
+// import java.util.*;
+
+// public class FirstClass {
+
+//     public static void main(String[] args){
+      
+//         Scanner sc =new Scanner(System.in);
+//         int number=sc.nextInt();
+//          for(int i=1; i <= 10;i++){
+//               System.out.println(number + "x" + i + "=" + (number * i));
+//          }
+
+
+//      sc.close();
+//     }
+    
+// }
+
+
+
+
+// !____________________calculater in java_____________________________!
+// import java.util.*;
+
+// public class FirstClass {
+
+//     public static void main(String[] args){
+      
+//         Scanner sc =new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+
+//         char operator=sc.next().charAt(0);
+       
+//         int result;
+//         switch (operator) {
+//             case '+':
+//                 result = a+b;
+//                 System.out.println("plus of two numbers" + result);
+//                 break;
+//             case '-':
+//                 result = a+b;
+//                 System.out.println("plus of two numbers" + result);
+//                 break;
+//             case '*':
+//                 result = a*b;
+//                 System.out.println("plus of two numbers" + result);
+//                 break;
+//             case '/':
+//             if(b == 0){
+//                System.out.println("error division by zero");
+//             }else{
+//                 result = a/b;
+//                 System.out.println("plus of two numbers" + result);
+//                 break;
+//             }
+               
+//             case '%':
+//                 result = a%b;
+//                 System.out.println("plus of two numbers" + result);
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+
+
+//      sc.close();
+//     }
+    
+// }
+
+
+
+
+// !________________ Print All Even Numbers till n _____________________________________________!
+
+
+// import java.util.*;
+
+// public class FirstClass {
+
+//     public static void main(String[] args){
+      
+//         Scanner sc =new Scanner(System.in);
+//         int a=sc.nextInt();
+    
+
+//         for(int i=0;i<=a;i++){
+//             if(i % 2 == 0){
+//                 System.out.println(i);
+//             }
+//         }
+       
+//      sc.close();
+//     }
+    
+// }
+
+
+
+
+
+// !_______________________ Print Student Numbers _____________________!
 
 import java.util.*;
 
 public class FirstClass {
 
     public static void main(String[] args){
-        double pi= 3.14 ;
+      
         Scanner sc =new Scanner(System.in);
-        float radius=sc.nextFloat();
-        double Area=pi * (radius*2);
-
-        System.out.println("Area of circle:" + Area);
-
+        int userChoice;
+        do{
+             System.out.println("marks driven system");
+             userChoice =sc.nextInt();
+             if(userChoice == 1){
+                System.out.println("inter marks");
+                int marks;
+                marks=sc.nextInt();
+              do{
+                marks = sc.nextInt();
+                if(marks >=90 ){
+                    System.out.println("This is Good");
+                }else if(89 >= marks && marks >= 60){
+                    System.out.println("This is also good");
+                }else if(59 >= marks && marks >= 0){
+                 System.out.println("This is Good as well");
+                }else if(marks != -1){
+                    System.out.println("invalid value please enter value greater then 1");
+                }
+              }while(marks != -1);
+             
+            }
+        }while(userChoice !=0);
+    
+    
      sc.close();
-    }
+    };
     
 }
